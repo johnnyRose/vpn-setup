b41ba8f8bab8e43a251e538484e07d0e8e76ae7b
John Rosewicz <john.rosewicz@gmail.com>
2016-08-14 03:11:48 +0000

    improvements

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vpn-setup
+
+_An automation script for setting up a simple PPTPD VPN server on a fresh install._
+
--- vpn-setup.bash
+++ vpn-setup.bash
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+# This bash script should set up a PPTPD VPN on a fresh install. 
+
+# Usage: sudo ./vpn-setup.bash
+
+# See https://www.digitalocean.com/community/tutorials/how-to-setup-your-own-vpn-with-pptp
+# for the original setup instructions (steps 1-4).
+
 secrets="/etc/ppp/chap-secrets"
 pptpdConfig="/etc/pptpd.conf"
+ip=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 
 sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install pptpd
 
-
-
 sudo echo "localip $1" >> $pptpdConfig
 sudo echo "remoteip 10.0.0.100-200" >> $pptpdConfig
-
-echo "Edit authentication information in $secrets"
-echo "syntax: username pptpd password *"
 
 sudo echo "ms-dns 8.8.8.8" >> $secrets
 sudo echo "ms-dns 8.8.4.4" >> $secrets
@@ -30,5 +33,7 @@ sudo iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERAD
 sudo iptables -I INPUT -s 10.0.0.0/8 -i ppp0 -j ACCEPT
 sudo iptables --append FORWARD --in-interface eth0 -j ACCEPT
 
-echo "complete"
+echo "VPN setup complete."
+echo "Edit authentication information in $secrets"
+echo "Syntax: username pptpd password *"
 
